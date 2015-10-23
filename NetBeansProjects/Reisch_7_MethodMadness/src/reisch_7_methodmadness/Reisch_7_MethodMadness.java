8131640622876036f25e9ed49fc99b06209b3524
sreisch3829 <sreisch3829@stu.neisd.net>
2015-10-23 10:26:54 -0500

    10/23/15

--- NetBeansProjects/Reisch_7_MethodMadness/src/reisch_7_methodmadness/Reisch_7_MethodMadness.java
+++ NetBeansProjects/Reisch_7_MethodMadness/src/reisch_7_methodmadness/Reisch_7_MethodMadness.java
@@ -32,51 +32,57 @@ public class Reisch_7_MethodMadness extends Application {
         GraphicsContext gc =canvas.getGraphicsContext2D();
         drawBackground(gc,0,0,300,250);
         drawBlueCircle(gc);
-        drawRect(gc,134,43,20,20);
         drawManyOvals(gc);
         drawOtherOvals(gc);
+        drawBiggestCircle(gc);
         drawMyCat(gc);
+        drawEyes(gc);
+        drawLittleButtons(gc);
         //drawShapes(gc);
         root.getChildren().add(canvas);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
             }
     private void drawBackground(GraphicsContext bg, int x, int y, int w, int h) {
-        bg.setFill(Color.FIREBRICK);
+        bg.setFill(Color.LIGHTSTEELBLUE);
         bg.fillRect(x, y, w, h);
     }
     
     private void drawBlueCircle(GraphicsContext blue) {
-        blue.setFill(Color.DARKRED);
-        blue.setStroke(Color.MOCCASIN);
+        blue.setFill(Color.PALETURQUOISE);
+        blue.setStroke(Color.TURQUOISE);
         blue.setLineWidth(3);
         blue.fillOval(115,10,60,60);
         blue.strokeOval(115,10,60,60); 
     }
     
-    private void drawRect(GraphicsContext amanda, int x, int y, int w, int h) {
-        amanda.setFill(Color.ORANGE);
-        amanda.fillRect(x, y, w, h);
-    }
     
     private void drawMyCat(GraphicsContext cat) {
-        cat.setFill(Color.WHITE);
-        cat.setStroke(Color.DARKGOLDENROD);
+        cat.setFill(Color.CYAN);
+        cat.setStroke(Color.DARKCYAN);
         cat.setLineWidth(5);
         cat.fillOval(108,70,80,80);
         cat.strokeOval(108,70,80,80);
+    }
+    
+    private void drawBiggestCircle(GraphicsContext big) {
+        big.setFill(Color.BLUE);
+        big.fillOval(100,150,100,100);
+        big.setStroke(Color.DARKBLUE);
+        big.setLineWidth(5);
+        big.strokeOval(100,150,100,100);
     }
     
     private void drawManyOvals(GraphicsContext many) {
         for (int i = 0; i < 300; i++) {
             if (i % 2 == 0){
                 many.fillOval(15, 40, 20, 20);
-                many.setFill(Color.DARKSALMON);
+                many.setFill(Color.NAVY);
             } else if (i % 3 == 0){
                 many.fillOval(35, 80, 20, 20);
-                many.setFill(Color.GOLD);
+                many.setFill(Color.CORNFLOWERBLUE);
             } else {
-                many.setFill(Color.CORNSILK);
+                many.setFill(Color.STEELBLUE);
                 many.fillOval(80, 65, 20, 20);
                 
             }
@@ -88,18 +94,31 @@ public class Reisch_7_MethodMadness extends Application {
         for (int i = 0; i < 300; i++) {
             if (i % 2 == 0){
                 other.fillOval(270, 40, 20, 20);
-                other.setFill(Color.DARKSALMON);
+                other.setFill(Color.NAVY);
             } else if (i % 3 == 0){
                 other.fillOval(245, 80, 20, 20);
-                other.setFill(Color.GOLD);
+                other.setFill(Color.CORNFLOWERBLUE);
             } else {
-                other.setFill(Color.CORNSILK);
+                other.setFill(Color.STEELBLUE);
                 other.fillOval(205, 65, 20, 20);
                 
             }
         }
         
     }
+    private void drawLittleButtons(GraphicsContext buttons) {
+                buttons.setFill(Color.BLACK);
+                buttons.fillRect(145, 110, 5, 5);
+                buttons.fillRect(145, 90, 5, 5);
+                buttons.fillRect(145, 100, 5, 5);
+    }
+    
+    private void drawEyes(GraphicsContext eyes) {
+        eyes.setFill(Color.BLACK);
+        eyes.fillOval(135,25,5,30);
+        eyes.fillOval(150,25,5,30);
+    }
+    
     static boolean drawManythings (int somenumber) {
         return (somenumber % 2 == 0);
     }
